0cdd3baec33068d4b23acff2629643a90fcd9abd
Natalia Byzova <nata22011997@gmail.com>
2023-04-03 01:07:55 +0300

    fix: protocol error

--- weather/src/data/api.ts
+++ weather/src/data/api.ts
@@ -1,2 +1,2 @@
 export const API_KEY = 'ee525304517e4bb09b9103606232903';
-export const API_LINK = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&aqi=no&q=`;
+export const API_LINK = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&aqi=no&q=`;
